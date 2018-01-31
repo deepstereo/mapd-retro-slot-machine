1fbec1c898343902baaa1a091c3ddf36adf85f32
Sergey Kozak <svkozak@icloud.com>
2018-01-31 12:39:16 -0500

    Added pickerview methods and images

--- retro-slot/ViewController.swift
+++ retro-slot/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var images = [UIImage()]
+    
+    @IBOutlet weak var spinner: UIPickerView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+    
 
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let image = images[row]
+        let imageView = UIImageView(image: image)
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 97
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        images = [
+            UIImage(named: "shroom")!,
+            UIImage(named: "invader")!,
+            UIImage(named: "star")!,
+            UIImage(named: "wizard")!,
+            UIImage(named: "joystick")!,
+            UIImage(named: "cherry")!,
+            UIImage(named: "heart")!
+        ]
+        
+        spinner.showsSelectionIndicator = true
     }
 
     override func didReceiveMemoryWarning() {
